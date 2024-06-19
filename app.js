86796da748fddcf5253f40e8e13512eada37b146
Kayhoon <kevin.huynh21@hotmail.com>
2024-06-19 18:27:31 +1000

    exercise 14

--- app.js
+++ app.js
@@ -259,6 +259,28 @@ Exercise 13: Retrieve the Last Array
 Complete Exercise 13 in the space below:
 */
 
+// const numArrays = [
+// 	[100, 5, 23],
+// 	[15, 21, 72, 9],
+// 	[45, 66],
+// 	[7, 81, 90]
+// ];
+
+// const numList = numArrays[numArrays.length - 1] 
+
+// console.log('Exercise 13 result:', numList);
+
+/*
+Exercise 14: Accessing within nested arrays
+
+1) Retrieve the number `66` from the `numArrays` array. As part of this process
+   do not alter the original `numArrays` array.
+
+2) Assign it to a variable called `num`.
+
+Complete Exercise 14 in the space below:
+*/
+
 const numArrays = [
 	[100, 5, 23],
 	[15, 21, 72, 9],
@@ -266,8 +288,9 @@ const numArrays = [
 	[7, 81, 90]
 ];
 
-const numList = numArrays[numArrays.length - 1] 
+const num = numArrays[2][1]
 
-console.log('Exercise 13 result:', numList);
+console.log('Exercise 14 result:', num);
+
 
 
